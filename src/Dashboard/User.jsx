869bd39b60f225c234e12644b441af9e991ebbbd
wuqufKsa <wuquf.ksa@gmail.com>
2024-06-27 11:20:33 +0300

    Add files via upload

---
+++ src/Dashboard/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const User = () => {
+  return (
+     <>
+      <div className='flex flex-row h-screen bg-white'>
+        
+      </div>
+     </>
+  )
+}
+
+export default User
